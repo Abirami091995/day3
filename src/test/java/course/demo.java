a5dcaf07e183801bb7ef9ab511feba2d2dce4f0b
Abirami091995 <64601932+Abirami091995@users.noreply.github.com>
2020-05-05 00:25:16 -0700

    Update demo.java

--- src/test/java/course/demo.java
+++ src/test/java/course/demo.java
@@ -3,6 +3,6 @@ package course;
 public class demo {
 public static void main(String[] args) {
 	System.out.println("completed");
-System.out.println("done my project")
+System.out.println("done my project");
 }
 }
